9ed114b37a48b1e2f78745cc885e982aea28a1ed
jeisux92 <jei.sum41@gmail.com>
2019-04-25 20:16:03 -0500

    prices

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,29 @@
 import React from 'react';
-const buildControls = props=>(
-    <div>
-        
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
+const controls = [
+    {
+        label: 'Salad', type: 'salad'
+    },
+    {
+        label: 'Bacon', type: 'bacon'
+    }, {
+        label: 'Cheese', type: 'cheese'
+    },
+    {
+        label: 'Meat', type: 'meat'
+    }
+]
+
+const buildControls = props => (
+    <div className={classes.BuildControls}>
+        <p>Current price:<strong> {props.price.toFixed(2)}</strong></p>
+        {controls.map(control => (<BuildControl
+            key={control.label}
+            label={control.label}
+            disabled={props.disabled[control.type]}
+            removed={props.ingredientRemoved.bind(this, control.type)}
+            added={props.ingredientAdded.bind(this, control.type)} />))}
     </div>
 );
 
