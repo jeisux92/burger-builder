6a97cc45489216818bc9bbcb395e3fa177176f9a
Gabriel Martinez <gabrielmartinez@lexicacorp.com>
2019-06-16 20:36:10 -0500

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout";
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder></BurgerBuilder>
-          </Layout>
+          <BrowserRouter>
+            <Switch>
+              <Route path="/" exact component={BurgerBuilder} />
+              <Route path="/checkout" component={Checkout} />
+            </Switch>
+          </BrowserRouter>
+        </Layout>
       </div>
     );
   }
