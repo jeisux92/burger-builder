76921bd4dee1bfebe9f48f9144eba0ba06c88d4e
jeisux92 <jei.sum41@gmail.com>
2019-08-07 22:50:09 -0500

    add middleware

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,7 +4,7 @@ import classes from "./NavigationItems.css";
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
     <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-    <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/orders" exact>Orders</NavigationItem>
   </ul>
 );
 
--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const logger = (store) => {
+    return next => {
+        return action => {
+            console.log("Middleware Dispatchig", action);
+            console.log("Middleware previous state", store.getState())
+            const result = next(action);
+            console.log("Middleware next state", store.getState())
+            return result;
+        }
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
